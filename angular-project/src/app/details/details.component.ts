a13e92c69d749cae93c64b7298bdb79be51e4482
Temo Korkotashvili <temo.korkotashvili@gmail.com>
2020-06-05 23:58:13 +0400

    Details Async Data

--- angular-project/src/app/details/details.component.ts
+++ angular-project/src/app/details/details.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { DetailsModel} from '../model/DetailsModel'
 import { DataService } from '../services/DataService';
+import { Data, ActivatedRoute } from '@angular/router';
+import {Observable} from 'rxjs'
+import { HttpClient } from '@angular/common/http';
+import { delay, share } from 'rxjs/operators';
 
 @Component({
   selector: 'app-details',
@@ -8,24 +12,29 @@ import { DataService } from '../services/DataService';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent implements OnInit {
+  personId:any;
   name:string;
   email:string;
   element:any;
-  id: number;
+  id:any;
   firstLetter:string;
-  
+
+  person$ :Observable<{}>;
   reciveElement($event:any){
     this.element=$event;
   }
-  constructor(private dataService:DataService) {
+  
+  constructor(private route:ActivatedRoute,private http:HttpClient) {
   }
   ngOnInit(): void {
-    let details = this.dataService.currentMessage.subscribe(m=>{
-      this.id=m.id;
-      this.name = m.name;
-      this.email = m.email;
-      this.firstLetter=this.name[0];
-    });
+    
+    this.personId =this.route.snapshot.paramMap.get('id');
+    this.person$=this.http.get('https://jsonplaceholder.typicode.com/users/'+this.personId).pipe(share());
+      // this.id=m.id;
+      // this.name = m.name;
+      // this.email = m.email;
+      // this.firstLetter=this.name[0];
+    // });
     // this.id=history.state.data.id; ესეც იყოს
     // this.name = history.state.data.name;
     // this.email = history.state.data.email;
